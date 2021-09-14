5224621e27d3dddeb94368bf75e0c961ed0c35b1
Xayvian <xmccumberpe@gmail.com>
2021-09-14 14:25:56 -0400

    Update TaylorSeries.java

--- src/main/java/TaylorSeries.java
+++ src/main/java/TaylorSeries.java
@@ -1,5 +1,34 @@
+package src.main.java;
+
 public class TaylorSeries {
 
     // YOUR CODE GOES HERE
+    
+    public TaylorSeries(){
+    }
+   
+    public static int factorial(int n){
+        if (n > 1){
+            return n*factorial(n-1);
+        }
+        return 1;
+    }
+    
+    public static int pow(int x, int n){
+        if (n>0){
+            return x*pow(x,n-1);
+        }
+        return 1;
+    }
+    
+        public static float approximateExp(int num, int iter){
+        int factorialAnswer = factorial(iter);
+        int powAnswer = pow(num,iter);
+        
+        if (iter > 0){
+            return (float)((double)powAnswer/(double)factorialAnswer)+approximateExp(num,iter-1);
 
+        }
+        return 1;
+    }
 }
